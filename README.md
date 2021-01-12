258129643855a681896bd08369ced91c6f4b37ed
Uriel Ramirez <aurbac@gmail.com>
2021-01-12 03:34:56 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Lambda functions for WorkMail Rules (Inbound/Outbound) with SAM
+
+This project contains source code and supporting files for a serverless application that you can deploy with the SAM CLI.
+The lambda functions 
+
+![Lambda functions for WorkMail Rules Inbound/Outbound with SAM](images/diagram.png)
+
+## Prerequisites
+
+- Work in your local or [AWS Cloud9](https://aws.amazon.com/cloud9/) environment with the following 
+- WorkMail Organization configured - [Creating an organization](https://docs.aws.amazon.com/workmail/latest/adminguide/add_new_organization.html)
+
+## Deploy the application
+
+The Serverless Application Model Command Line Interface (SAM CLI) is an extension of the AWS CLI that adds functionality for building and testing Lambda applications. It uses Docker to run your functions in an Amazon Linux environment that matches Lambda. It can also emulate your application's build environment and API.
+
+To use the SAM CLI, you need the following tools.
+
+* SAM CLI - [Install the SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html)
+* [Python 3 installed](https://www.python.org/downloads/)
+* Docker - [Install Docker community edition](https://hub.docker.com/search/?type=edition&offering=community)
+
+
+To build and deploy your application for the first time, run the following in your shell:
+
+```bash
+sam build --use-container
+sam deploy --guided
+```
+
+The first command will build the source of your application. The second command will package and deploy your application to AWS, with a series of prompts:
+
+* **Stack Name**: The name of the stack to deploy to CloudFormation. This should be unique to your account and region, and a good starting point would be something matching your project name.
+* **AWS Region**: The AWS region you want to deploy your app to.
+* **Confirm changes before deploy**: If set to yes, any change sets will be shown to you before execution for manual review. If set to no, the AWS SAM CLI will automatically deploy application changes.
+* **Allow SAM CLI IAM role creation**: Many AWS SAM templates, including this example, create AWS IAM roles required for the AWS Lambda function(s) included to access AWS services. By default, these are scoped down to minimum required permissions. To deploy an AWS CloudFormation stack which creates or modified IAM roles, the `CAPABILITY_IAM` value for `capabilities` must be provided. If permission isn't provided through this prompt, to deploy this example you must explicitly pass `--capabilities CAPABILITY_IAM` to the `sam deploy` command.
+* **Save arguments to samconfig.toml**: If set to yes, your choices will be saved to a configuration file inside the project, so that in the future you can just re-run `sam deploy` without parameters to deploy changes to your application.
+
+You can find your **2 Lambda functions** to use in the output values displayed after deployment.
+
+## Configure WorkMail Rules (Inbound/Outbound)
+
+[Configuring AWS Lambda for Amazon WorkMail](https://docs.aws.amazon.com/workmail/latest/adminguide/lambda.html) using the functions created with SAM.
---
+++ code/app.py
@@ -0,0 +1,205 @@
+import os
+import json
+import boto3
+import datetime
+from datetime import datetime
+from botocore.exceptions import ClientError
+
+def inbound_handler(event, context):
+    print(event)
+    
+    DOMAIN = os.environ['DOMAIN']
+    MESSAGES_TABLE = os.environ['MESSAGES_TABLE']
+    
+    to_drop = False
+    if event['envelope']['mailFrom']['address'].find('@'+DOMAIN)==-1:
+        to_drop = True
+    
+    try:
+        fromAddress = event['envelope']['mailFrom']['address']
+        subject = event['subject']
+        flowDirection = event['flowDirection']
+        messageId = event['messageId']
+
+        print(f"Received {flowDirection} email from {fromAddress} with Subject {subject}")
+        print(f"Use Message ID:{messageId} to retrieve full content of the email") # For more information, see: https://docs.aws.amazon.com/workmail/latest/adminguide/lambda-content.html
+
+    except Exception as e:
+        # Send some context about this error to Lambda Logs
+        print(e)
+        raise e
+
+    # Return value is ignored when Lambda is configured asynchronously at Amazon WorkMail
+    # For more information, see https://docs.aws.amazon.com/workmail/latest/adminguide/lambda.html
+    
+    result = {
+          'actions': [
+          {
+            'allRecipients': True,                  # For all recipients
+            'action' : { 'type' : 'DEFAULT' }       # let the email be sent normally
+          }
+        ]}
+    
+    if to_drop:
+        result['actions'][0]['action']['type'] = 'DROP'
+    print(result)
+    
+    try:
+        dynamodb = boto3.client('dynamodb')
+        item = {
+            'messageId': { 'S': messageId },
+            'fromAddress': { 'S': fromAddress },
+            'flowDirection': { 'S': flowDirection },
+            'subject': { 'S': subject },
+            'recipients': { 'S': json.dumps(event['envelope']['recipients']) },
+            'response': { 'S': json.dumps(result) },
+            'datetimeMessage': { 'S': datetime.utcnow().isoformat() }
+        }
+        response = dynamodb.put_item(
+            TableName=MESSAGES_TABLE,
+            Item=item
+        )
+    except ClientError as e:
+        print(e)
+    
+    
+    return result
+
+
+def outbound_handler(event, context):
+    print(event)
+    
+    DOMAIN = os.environ['DOMAIN']
+    MESSAGES_TABLE = os.environ['MESSAGES_TABLE']
+    
+    drops = {
+      'action' : {'type': 'DROP'},
+      'recipients' : []
+    }
+    for recipient in event['envelope']['recipients']:
+        if recipient['address'].find('@'+DOMAIN)==-1:
+            drops['recipients'].append(recipient['address'])
+            
+    try:
+        fromAddress = event['envelope']['mailFrom']['address']
+        subject = event['subject']
+        flowDirection = event['flowDirection']
+        messageId = event['messageId']
+
+        print(f"Received {flowDirection} email from {fromAddress} with Subject {subject}")
+        print(f"Use Message ID:{messageId} to retrieve full content of the email") # For more information, see: https://docs.aws.amazon.com/workmail/latest/adminguide/lambda-content.html
+
+    except Exception as e:
+        # Send some context about this error to Lambda Logs
+        print(e)
+        raise e
+
+    # Return value is ignored when Lambda is configured asynchronously at Amazon WorkMail
+    # For more information, see https://docs.aws.amazon.com/workmail/latest/adminguide/lambda.html
+    
+    result = {
+          'actions': [
+          {
+            'allRecipients': True,                  # For all recipients
+            'action' : { 'type' : 'DEFAULT' }       # let the email be sent normally
+          }
+        ]}
+    
+    if len(drops['recipients'])>0:
+        result['actions'].append(drops)
+    print(result)
+    try:
+        dynamodb = boto3.client('dynamodb')
+        item = {
+            'messageId': { 'S': messageId },
+            'fromAddress': { 'S': fromAddress },
+            'flowDirection': { 'S': flowDirection },
+            'subject': { 'S': subject },
+            'recipients': { 'S': json.dumps(event['envelope']['recipients']) },
+            'response': { 'S': json.dumps(result) },
+            'datetimeMessage': { 'S': datetime.utcnow().isoformat() }
+        }
+        response = dynamodb.put_item(
+            TableName=MESSAGES_TABLE,
+            Item=item
+        )
+    except ClientError as e:
+        print(e)
+    return result
+
+
+"""
+Hello world example for AWS WorkMail
+
+Parameters
+----------
+event: dict, required
+    AWS WorkMail Message Summary Input Format
+    For more information, see https://docs.aws.amazon.com/workmail/latest/adminguide/lambda.html
+
+    {
+        "summaryVersion": "2019-07-28",                              # AWS WorkMail Message Summary Version
+        "envelope": {
+            "mailFrom" : {
+                "address" : "from@domain.test"                       # String containing from email address
+            },
+            "recipients" : [                                         # List of all recipient email addresses
+               { "address" : "recipient1@domain.test" },
+               { "address" : "recipient2@domain.test" }
+            ]
+        },
+        "sender" : {
+            "address" :  "sender@domain.test"                        # String containing sender email address
+        },
+        "subject" : "Hello From Amazon WorkMail!",                   # String containing email subject (Truncated to first 256 chars)"
+        "messageId": "00000000-0000-0000-0000-000000000000",         # String containing message id for retrieval using workmail flow API
+        "invocationId": "00000000000000000000000000000000",          # String containing the id of this lambda invocation. Useful for detecting retries and avoiding duplication
+        "flowDirection": "INBOUND",                                  # String indicating direction of email flow. Value is either "INBOUND" or "OUTBOUND"
+        "truncated": false                                           # boolean indicating if any field in message was truncated due to size limitations
+    }
+
+context: object, required
+    Lambda Context runtime methods and attributes
+
+    Attributes
+    ----------
+
+    context.aws_request_id: str
+         Lambda request ID
+    context.client_context: object
+         Additional context when invoked through AWS Mobile SDK
+    context.function_name: str
+         Lambda function name
+    context.function_version: str
+         Function version identifier
+    context.get_remaining_time_in_millis: function
+         Time in milliseconds before function times out
+    context.identity:
+         Cognito identity provider context when invoked through AWS Mobile SDK
+    context.invoked_function_arn: str
+         Function ARN
+    context.log_group_name: str
+         Cloudwatch Log group name
+    context.log_stream_name: str
+         Cloudwatch Log stream name
+    context.memory_limit_in_mb: int
+        Function memory
+
+        https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+Returns
+-------
+Amazon WorkMail Sync Lambda Response Format. For more information, see https://docs.aws.amazon.com/workmail/latest/adminguide/lambda.html#synchronous-schema
+    return {
+      'actions': [                                              # Required, should contain at least 1 list element
+      {
+        'action' : {                                            # Required
+          'type': 'string',                                     # Required. For example: "BOUNCE", "DEFAULT". For full list of valid values, see https://docs.aws.amazon.com/workmail/latest/adminguide/lambda.html#synchronous-schema
+          'parameters': { <various> }                           # Optional. For bounce, <various> can be {"bounceMessage": "message that goes in bounce mail"}
+        },
+        'recipients': list of strings,                          # Optional if allRecipients is present. Indicates list of recipients for which this action applies.
+        'allRecipients': boolean                                # Optional if recipients is present. Indicates whether this action applies to all recipients
+      }
+    ]}
+
+"""
